238f20d0cdff4f6450adba425beb00755b478639
Aayush Kumar Bhat <kumarbhataayush@gmail.com>
2025-12-02 15:41:57 +0530

    keep alive script for render services - github actions

---
+++ workflow/pinger.js
@@ -0,0 +1,21 @@
+const https = require("https");
+
+const services = [
+  "https://taskflow-autth.onrender.com",
+
+  "https://taskflow-socket.onrender.com",
+];
+
+function pingServices() {
+  services.forEach((url) => {
+    https
+      .get(url, (res) => {
+        console.log(`Pinged ${url} - Status Code: ${res.statusCode}`);
+      })
+      .on("error", (e) => {
+        console.error(`Error pinging ${url}: ${e.message}`);
+      });
+  });
+}
+
+pingServices();
